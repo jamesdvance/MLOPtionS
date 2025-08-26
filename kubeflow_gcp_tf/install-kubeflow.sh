9b31a8a5ae240738a497e485d17b679515e7ba0a
James Vance <james.dvance@gmail.com>
2025-08-26 17:50:56 -0400

    updates kubeflow install script

--- kubeflow_gcp_tf/install-kubeflow.sh
+++ kubeflow_gcp_tf/install-kubeflow.sh
@@ -44,9 +44,9 @@ kubectl wait --for=condition=ready pod -l app=webhook -n cert-manager --timeout=
 
 # Install Istio
 echo "Installing Istio..."
-kubectl apply -k common/istio-1-17/istio-crds/base | kubectl apply -f -
-kubectl apply -k common/istio-1-17/istio-namespace/base | kubectl apply -f -
-kubectl apply -k common/istio-1-17/istio-install/base | kubectl apply -f -
+kubectl apply -k common/istio/istio-crds/base
+kubectl apply -k common/istio/istio-namespace/base
+kubectl apply -k common/istio/istio-install/base
 
 # Wait for Istio to be ready
 echo "Waiting for Istio to be ready..."
@@ -54,32 +54,32 @@ kubectl wait --for=condition=ready pod -l app=istiod -n istio-system --timeout=3
 
 # Install Dex
 echo "Installing Dex..."
-kubectl apply -k  common/dex/overlays/istio | kubectl apply -f -
+kubectl apply -k  common/dex/overlays/istio
 
 # Install OIDC AuthService
-echo "Installing OIDC AuthService..."
-kubectl apply -k  common/oidc-authservice/base | kubectl apply -f -
+echo "Installing Oath2 Proxy.."
+kubectl apply -k  common/oauth2-proxy/base
 
 # Install Knative
 echo "Installing Knative..."
-kubectl apply -k  common/knative/knative-serving/overlays/gateways | kubectl apply -f -
-kubectl apply -k  common/istio-1-17/cluster-local-gateway/base | kubectl apply -f -
+kubectl apply -k  common/knative/knative-serving/overlays/gateways
+kubectl apply -k  common/istio/cluster-local-gateway/base
 
 # Install Kubeflow Namespace
 echo "Installing Kubeflow Namespace..."
-kubectl apply -k  common/kubeflow-namespace/base | kubectl apply -f -
+kubectl apply -k  common/kubeflow-namespace/base
 
 # Install Kubeflow Roles
 echo "Installing Kubeflow Roles..."
-kubectl apply -k  common/kubeflow-roles/base | kubectl apply -f -
+kubectl apply -k  common/kubeflow-roles/base
 
 # Install Kubeflow Istio Resources
 echo "Installing Kubeflow Istio Resources..."
-kubectl apply -k  common/istio-1-17/kubeflow-istio-resources/base | kubectl apply -f -
+kubectl apply -k  common/istio/kubeflow-istio-resources/base
 
 # Install Kubeflow Pipelines
 echo "Installing Kubeflow Pipelines..."
-kubectl apply -k  apps/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
+kubectl apply -k  applications/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user
 
 # Apply custom database configuration
 echo "Applying custom database configuration..."
@@ -87,47 +87,47 @@ kubectl apply -f ../kubeflow-deployment.yaml
 
 # Install Katib
 echo "Installing Katib..."
-kubectl apply -k  apps/katib/upstream/installs/katib-with-kubeflow | kubectl apply -f -
+kubectl apply -k  applications/katib/upstream/installs/katib-with-kubeflow
 
 # Install Central Dashboard
 echo "Installing Central Dashboard..."
-kubectl apply -k  apps/centraldashboard/upstream/overlays/kserve | kubectl apply -f -
+kubectl apply -k  applications/centraldashboard/upstream/overlays/kserve
 
 # Install Admission Webhook
 echo "Installing Admission Webhook..."
-kubectl apply -k  apps/admission-webhook/upstream/overlays/cert-manager | kubectl apply -f -
+kubectl apply -k  applications/admission-webhook/upstream/overlays/cert-manager
 
 # Install Notebook Controller
 echo "Installing Notebook Controller..."
-kubectl apply -k  apps/jupyter/notebook-controller/upstream/overlays/kubeflow | kubectl apply -f -
+kubectl apply -k  applications/jupyter/notebook-controller/upstream/overlays/kubeflow
 
 # Install Jupyter Web App
 echo "Installing Jupyter Web App..."
-kubectl apply -k  apps/jupyter/jupyter-web-app/upstream/overlays/istio | kubectl apply -f -
+kubectl apply -k  applications/jupyter/jupyter-web-app/upstream/overlays/istio
 
 # Install Profiles + KFAM
 echo "Installing Profiles + KFAM..."
-kubectl apply -k  apps/profiles/upstream/overlays/kubeflow | kubectl apply -f -
+kubectl apply -k  applications/profiles/upstream/overlays/kubeflow
 
 # Install Volumes Web App
 echo "Installing Volumes Web App..."
-kubectl apply -k  apps/volumes-web-app/upstream/overlays/istio | kubectl apply -f -
+kubectl apply -k  applications/volumes-web-app/upstream/overlays/istio
 
 # Install Tensorboards Controller
 echo "Installing Tensorboards Controller..."
-kubectl apply -k  apps/tensorboard/tensorboard-controller/upstream/overlays/kubeflow | kubectl apply -f -
+kubectl apply -k  applications/tensorboard/tensorboard-controller/upstream/overlays/kubeflow
 
 # Install Tensorboards Web App
 echo "Installing Tensorboards Web App..."
-kubectl apply -k  apps/tensorboard/tensorboards-web-app/upstream/overlays/istio | kubectl apply -f -
+kubectl apply -k  applications/tensorboard/tensorboards-web-app/upstream/overlays/istio
 
 # Install Training Operator
 echo "Installing Training Operator..."
-kubectl apply -k  apps/training-operator/upstream/overlays/kubeflow | kubectl apply -f -
+kubectl apply -k  applications/training-operator/upstream/overlays/kubeflow
 
 # Install User Namespace
 echo "Installing User Namespace..."
-kubectl apply -k  common/user-namespace/base | kubectl apply -f -
+kubectl apply -k  common/user-namespace/base
 
 cd ..
 
